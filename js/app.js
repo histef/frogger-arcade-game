37ed15c56cb74b19ab9413f56fc8925f316e8cb7
Stefanie Chin <hi.stef.c@gmail.com>
2018-06-03 13:31:46 -0700

    add collision function

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 //TODO: enemy initial location and speed goes here
-    this.x = Math.random()*250;
+    this.x = Math.floor(Math.random()*-400);
 };
 
 // Update the enemy's position, required method for game
@@ -13,11 +13,27 @@ Enemy.prototype.update = function(dt) {
     // all computers.
 
 //TODO: updates enemy location and handles collision with Player
-    this.x += Math.floor(Math.random()*(Math.random()*10)); //changes speed of bugs
+    this.x += Math.floor(Math.random()*(7 - 1)+1);
+    /*console.log(`${this.y} , ${this.x}`) *///changes speed of bugs
     if(this.x > 525){
-        this.x = -100;
+        this.x = Math.floor(Math.random()*-400);
     }
+    this.checkCollisions();
 };
+
+//checks collision
+Enemy.prototype.checkCollisions = function(){
+    if(this.y === player.y){
+        if((this.x >= 0 && this.x < 20) && player.x === 0|| 
+        (this.x > 80 && this.x < 100) && player.x === 100 ||
+        (this.x > 120 && this.x < 200) && player.x === 200 ||
+        (this.x > 220 && this.x < 300) && player.x === 300 ||
+        (this.x > 320 && this.x < 400) && player.x === 400){
+        player.x = 200;
+        player.y = 400;
+    }
+    }
+}
 
 // Draw the Enemy on the screen
 Enemy.prototype.render = function() {
@@ -34,10 +50,7 @@ let Player = function() {
 
 Player.prototype.update = function(dt) {
 //TODO: updates player location and handles collision
-/*    if(this.x && this.y === enemy.x && enemy.y){
-        this.x = 200;
-        this.y = 400; 
-    }*/
+
 }
 
 Player.prototype.render = function() {
@@ -53,10 +66,10 @@ Player.prototype.handleInput = function(keyCode){
         this.x += 100;
         console.log(this.x);
     } else if (keyCode === 'up' && this.y > 0){
-        this.y -= 60;
+        this.y -= 85;
         console.log(this.y);
     } else if (keyCode === 'down' && this.y < 400){
-        this.y += 60;
+        this.y += 85;
         console.log(this.y);
     }
 
@@ -64,13 +77,21 @@ Player.prototype.handleInput = function(keyCode){
     if (this.y < 0){
         this.x = 200;
         this.y = 400;
+
+/* what to bind this too here??
+        setTimeout(function(){
+        this.x = 200;
+        this.y = 400;
+    }, 100);
+*/
         //add point pop up ()
+        //increase enemy speed function()
     }
 }
 
 //Instantiate your objects.
 //Place all enemy objects in an array called allEnemies
-const enemyYPosition = [60, 60, 140, 220, 220];
+const enemyYPosition = [60, 60, 145, 230];
 const allEnemies = [];
 enemyYPosition.forEach(function(en){
     let enemy = new Enemy();
